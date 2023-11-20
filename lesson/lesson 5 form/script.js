442ab2ee0cb1b31853c3e877a52ce72c0b4e8baa
Safarova1 <samasafarova1998@gmail.com>
2023-11-20 11:53:20 +0400

    l

--- lesson/lesson 5 form/script.js
+++ lesson/lesson 5 form/script.js
@@ -1,34 +1,63 @@
+// --------------------------------------------------------
+// zadaca1 - inputun deyerini objecte yazdim
+
+
+// let send = document.querySelector('#send')
+// let form = document.querySelector('form')
+
+
+// let inputs = document.querySelector('.userName')
+
+
+
+// form.addEventListener('submit', function (ev) {
+//     ev.preventDefault()  //sehife yenilenir
+   
+//     //1ci usul
+//     let formDate = new FormData(this)
+//     let forInfo = [...formDate]
+
+//     let obj = {}
+//     forInfo.forEach((item) => {
+//         obj[item[0]] = item[1]
+//     })
+
+//     // console.log([...formData.entries()])--2ci usul
+
+//     console.log(forInfo)
+//     console.log('rabotayet')
+//     console.log(obj)
+// })
+
+
+
+
+
+// --------------------------------------------------------
+// zadaca 2 -deyerini
+
 let send = document.querySelector('#send')
+let list = document.querySelector('#list')
+
 let form = document.querySelector('form')
 
-
-let inputs = document.querySelector('.userName')
-
+let inputs = document.querySelectorAll('.userName')
 
 
-form.addEventListener('submit', function (ev) {
-    ev.preventDefault()  //sehife yenilenir
-
-    let formDate = new FormData(this)
-    let forInfo = [...formDate]
-
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault()
+    let formData = new FormData(this)
+    let formInfo = [...formData]
     let obj = {}
-
-    forInfo.forEach((item) => {
+    console.log([...formData.entries()])
+    formInfo.forEach((item) => {
         obj[item[0]] = item[1]
     })
 
-
-    console.log(forInfo)
-    console.log('rabotayet')
-    console.log(obj)
+    for (const key in obj) {       
+        const element = obj[key];          
+        let p = document.createElement('p')
+        p.innerHTML = `<span>${key}</span>---<span>${element}</span>`
+        list.appendChild(p)
+}
 })
-
-
-
-
-
-
-
-
-
