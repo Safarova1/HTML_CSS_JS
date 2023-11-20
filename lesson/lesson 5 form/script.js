801756068da7cfa7768118b22e69881c0c1734b2
Safarova1 <samasafarova1998@gmail.com>
2023-11-20 12:21:15 +0400

    l

--- lesson/lesson 5 form/script.js
+++ lesson/lesson 5 form/script.js
@@ -12,7 +12,7 @@
 
 // form.addEventListener('submit', function (ev) {
 //     ev.preventDefault()  //sehife yenilenir
-   
+
 //     //1ci usul
 //     let formDate = new FormData(this)
 //     let forInfo = [...formDate]
@@ -82,25 +82,45 @@ let inputs = document.querySelectorAll('.userName')
 
 
 
-form.addEventListener('submit', function(ev) {
-    ev.preventDefault()
-    let formData = new FormData(this)
-    let formInfo = [...formData]
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault()   // sehife yenilenmesin
+    let formData = new FormData(this)  //formun elementlerini goturdum
+    let formInfo = [...formData]  //formun elementlerini massive yaziram
+
+
+
+    //massivin 1ci elementini duzeliw etmek 
+    // _1ci herfi boyukle
+    // - tire varsa yox edirem
     formInfo = formInfo.map((item) => {
-        let element = item
-        return element
+
+        if (item[0].includes('-')) {
+
+            let arr = [item[0].split('-').join('_'), item[1]]
+            let el = [arr[0][0].toUpperCase() + arr[0].slice(1), arr[1]]
+            return el
+
+        } else {
+
+            let el = [item[0][0].toUpperCase() + item[0].slice(1), item[1]]
+            return el
+        }
     })
+
+    console.log(formInfo)
+
+
+
     let obj = {}
-    
     formInfo.forEach((item) => {
         obj[item[0]] = item[1]
     })
 
-    for (const key in obj) {       
-            const element = obj[key];          
-            let p = document.createElement('p')
-            p.innerHTML = `<span>${key}</span><span>${element}</span>`
-            list.appendChild(p)
+    for (const key in obj) {
+        const element = obj[key];
+        let p = document.createElement('p')
+        p.innerHTML = `<span>${key}</span><span>${element}</span>`
+        list.appendChild(p)
     }
 })
 
