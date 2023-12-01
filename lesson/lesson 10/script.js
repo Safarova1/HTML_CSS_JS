08ad4d8793df74a10601df7f550ff7e2d67984b4
Safarova1 <samasafarova1998@gmail.com>
2023-12-01 12:24:26 +0400

    lesson 10

---
+++ lesson/lesson 10/script.js
@@ -0,0 +1,24 @@
+let vid = document.getElementById("myVideo"); 
+let body = document.querySelector("body")
+
+
+// body.addEventListener('mousemove', function(){
+//     vid.muted =false
+// })
+
+
+function widthLarge(){
+    vid.width="400"
+}
+function widthSmall(){
+    vid.width="100"
+}
+
+
+function qosmaq(){
+    vid.play()
+}
+
+function dayandirmaq(){
+    vid.pause()
+}
