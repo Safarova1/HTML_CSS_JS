2781dfa4a55f0602cfd4752599b10c587be29e9a
Safarova1 <samasafarova1998@gmail.com>
2023-12-01 13:57:04 +0400

    l 10

--- lesson/lesson 10/script.js
+++ lesson/lesson 10/script.js
@@ -1,4 +1,4 @@
-let vid = document.getElementById("myVideo"); 
+let vid = document.getElementById("myVideo");
 let body = document.querySelector("body")
 
 
@@ -7,18 +7,39 @@ let body = document.querySelector("body")
 // })
 
 
-function widthLarge(){
-    vid.width="400"
+function widthLarge() {
+    vid.width = "400"
 }
-function widthSmall(){
-    vid.width="100"
+function widthSmall() {
+    vid.width = "100"
 }
 
 
-function qosmaq(){
+function qosmaq() {
     vid.play()
 }
 
-function dayandirmaq(){
+function dayandirmaq() {
     vid.pause()
 }
+
+
+let myAudio = document.getElementById("myAudio");
+let mute_aud = document.getElementById('mute_aud')
+
+
+let mute = true
+mute_aud.addEventListener("click", function () {
+    if (mute) {
+        myAudio.muted = true
+        mute_aud.innerText='muted'
+        mute = false
+        console.log(myAudio.muted)
+    } else {
+        myAudio.muted = false
+        mute_aud.innerText='Not muted'
+        mute = true   
+        console.log(myAudio.muted)
+    }
+})
+
